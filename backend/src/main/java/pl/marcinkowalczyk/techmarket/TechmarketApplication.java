8389f154d302f00fefdb7b428bcc2f8a53fc75da
Marcin Kowalczyk <mkczyk@gmail.com>
2020-10-18 19:44:39 +0200

    Project skeleton of backend

---
+++ backend/src/main/java/pl/marcinkowalczyk/techmarket/TechmarketApplication.java
@@ -0,0 +1,13 @@
+package pl.marcinkowalczyk.techmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechmarketApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/pl/marcinkowalczyk/techmarket/TechmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcinkowalczyk.techmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
